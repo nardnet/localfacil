a2e3ac43efedda7f56584a5fd7eb37a8e31fcd92
nardnet <nardnet@gmail.com>
2016-12-27 22:54:52 -0200

    27/12/2016

---
+++ localfacil/src/main/java/br/com/localfacilonline/LocalfacilApplication.java
@@ -0,0 +1,12 @@
+package br.com.localfacilonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalfacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalfacilApplication.class, args);
+	}
+}
