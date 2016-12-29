1ae8d35ca5ba0152cfc8f2357008f87f0f58d1c1
nardnet <nardnet@gmail.com>
2016-12-28 23:55:04 -0200

    28/12/2016

--- localfacil/src/main/java/br/com/localfacilonline/LocalfacilApplication.java
+++ localfacil/src/main/java/br/com/localfacilonline/LocalfacilApplication.java
@@ -2,9 +2,10 @@ package br.com.localfacilonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LocalfacilApplication {
+public class LocalfacilApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocalfacilApplication.class, args);
